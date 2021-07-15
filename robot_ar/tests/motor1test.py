e129654c765566827ba7fcfc718f4e25d51b6f6a
Ubuntu <ubuntu@ubuntu.fios-router.home>
2021-07-15 19:42:22 +0000

    Small changes

--- robot_ar/tests/motor1test.py
+++ robot_ar/tests/motor1test.py
@@ -5,7 +5,8 @@ from adafruit_motorkit import MotorKit
 kit = MotorKit(i2c=board.I2C())
 
 
-kit.motor1.throttle = 1
-kit.motor2.throttle = 1
+kit.motor1.throttle = 0.5
+kit.motor2.throttle = 0.5
 time.sleep(0.5)
 kit.motor1.throttle = 0
+kit.motor2.throttle = 0
