e5996186fa3afcad854525bbae64dc375d3396e0
ArgenisW <argen1swong@gmail.com>
2021-07-15 15:30:48 -0400

    Update motor1test

--- robot_ar/tests/motor1test.py
+++ robot_ar/tests/motor1test.py
@@ -5,6 +5,7 @@ from adafruit_motorkit import MotorKit
 kit = MotorKit(i2c=board.I2C())
 
 
-kit.motor1.throttle = -0.5
+kit.motor1.throttle = 1
+kit.motor2.throttle = 1
 time.sleep(0.5)
 kit.motor1.throttle = 0
