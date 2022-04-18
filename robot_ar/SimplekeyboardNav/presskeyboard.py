32239a887abaaa28aa816446470909650764f2ad
ArgenisW <argen1swong@gmail.com>
2022-04-17 23:11:09 -0400

    Update presskeyboard.py

--- robot_ar/SimplekeyboardNav/presskeyboard.py
+++ robot_ar/SimplekeyboardNav/presskeyboard.py
@@ -1,7 +1,8 @@
 
 import time
 import motor_class
-import keyboard
+from pynput.keyboard import listener
+
 
 
 LEFT_TRIM = 0
@@ -12,19 +13,31 @@ RIGHT_TRIM = 0
 
 robot = motor_class.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
-while True:
-   try:
-      if keyboard.is_pressed('w'):
-         robot.forward(0.3, 1)
-         time.sleep(3)
-      elif keyboard.is_pressed('A'):
-         robot.left(0.3, 1)
-         time.sleep(3)
-      elif keyboard.is_pressed('D'):
-         robot.right(0.3, 1)
-         time.sleep(3)
-      elif keyboard.is_pressed('S'):
-         robot.backward(0.3, 1)
-         time.sleep(3)
-   except:
-      break
+with Listener(on_press=on_press, on_release=on_release) as listener:
+   listener.join()
+
+   def on_press(key):
+      print('key pressed')
+
+   def on_release(key):
+      print('key released')
+
+
+
+
+# while True:
+#    try:
+#       if keyboard.is_pressed('w'):
+#          robot.forward(0.3, 1)
+#          time.sleep(3)
+#       elif keyboard.is_pressed('A'):
+#          robot.left(0.3, 1)
+#          time.sleep(3)
+#       elif keyboard.is_pressed('D'):
+#          robot.right(0.3, 1)
+#          time.sleep(3)
+#       elif keyboard.is_pressed('S'):
+#          robot.backward(0.3, 1)
+#          time.sleep(3)
+#    except:
+#       break
