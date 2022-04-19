b47b618b61b020d443788e1ae3373f6ab313c1fe
ArgenisW <argen1swong@gmail.com>
2022-04-18 23:38:00 -0400

    Update basicNav.py

--- robot_ar/AutonomousNav/basicNav.py
+++ robot_ar/AutonomousNav/basicNav.py
@@ -3,16 +3,6 @@ import serial
 import time
 
 
-
-
-# Arduino Serial Port Listener
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.reset_input_buffer()
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-
 # Basic navigation using line as a input
 
 LEFT_TRIM = 0
@@ -22,22 +12,31 @@ RIGHT_TRIM = 0
 
 robot = motor_class.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
+# Arduino Serial Port Listener
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser.reset_input_buffer()
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+
+        # Define the variables that will be used for navigation
+        int(line)
+        range = 30
+
+        # First conditional statement to move the robot forward 
+        if range >= line:
+            print("robot forward")
+            time.sleep(1)
+
+        # Second conditional statement to move the robot to the right 
+        elif range <= line:
+            print("robot going right")
+            time.sleep(1)
+
+        elif 30 == 'E' | 5 <= line: #Stops the robot
+            robot.stop()
+            break
+
+
 #Loop that runs the code that will register the keyboard entry
-while True:
-    # Define the variables that will be used for navigation
-    int(line)
-    range = 30
-
-    # First conditional statement to move the robot forward 
-    if range >= line:
-        print("robot forward")
-        time.sleep(1)
-
-    # Second conditional statement to move the robot to the right 
-    elif range <= line:
-        print("robot going right")
-        time.sleep(1)
-
-    elif 30 == 'E' | 5 <= line: #Stops the robot
-        robot.stop()
-        break
