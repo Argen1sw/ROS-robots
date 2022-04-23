e06a55f96ffb9520001d5ea9286279d3e1e82ab5
ArgenisW <argen1swong@gmail.com>
2022-04-23 00:13:42 -0400

    Updated basicNav.py

--- robot_ar/AutonomousNav/basicNav.py
+++ robot_ar/AutonomousNav/basicNav.py
@@ -15,13 +15,43 @@ robot = motor_class.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
 # Arduino Serial Port Listener
 
+# if __name__ == '__main__':
+#     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+#     ser.reset_input_buffer()
+
+#     while True:
+#         ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+#         ser.reset_input_buffer()
+#         arduino = ser.read()
+#         if arduino != b'':
+#             arduino_data = int.from_bytes(arduino, byteorder='big')
+#             print(arduino_data)
+#             range = 30
+#             #First conditional statement to move the robot forward 
+#             print("starting the navigation loop")
+#             if arduino_data <= 5: #Stops the robot
+#                 print("robot will stop" + str(arduino_data))
+#                 robot.stop()
+#                 break
+
+#             elif range <= arduino_data:
+#                 print("robot forward" + str(arduino_data))
+#                 robot.forward(0.2, 0.2)
+#                 time.sleep(1)
+
+#             # Second conditional statement to move the robot to the right 
+#             elif range >= arduino_data:
+#                 print("robot going right" + str(arduino_data))
+#                 robot.right(0.2, 0.5)
+#                 time.sleep(1)
+
+#         time.sleep(2)
 
 
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.reset_input_buffer()
 
-    while True:
+def CollectUltrasonic():
+
+    if __name__ == '__main__':
         ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
         ser.reset_input_buffer()
         arduino = ser.read()
@@ -34,6 +64,7 @@ if __name__ == '__main__':
             if arduino_data <= 5: #Stops the robot
                 print("robot will stop" + str(arduino_data))
                 robot.stop()
+                return 0
 
             elif range <= arduino_data:
                 print("robot forward" + str(arduino_data))
@@ -46,52 +77,9 @@ if __name__ == '__main__':
                 robot.right(0.2, 0.5)
                 time.sleep(1)
 
-
         time.sleep(2)
 
 
-
-
-
-
-
-
-
-# def CollectUltrasonic():
-
-#     print("Inizializing Raspberry connection with Arduino")
-#     arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
-#     arduino.reset_input_buffer()
-#     # Define the variables that will be used for navigation
-#     range = 30
-#     if arduino.in_waiting > 0:
-#         arduino_data = arduino.readline().decode('utf-8')
-#         print(arduino_data)
-#         distance = int(arduino_data)
-
-#         # First conditional statement to move the robot forward 
-#         print("starting the navigation loop")
-#         if range >= distance:
-#             print("robot forward" + distance)
-#             time.sleep(1)
-
-#         # Second conditional statement to move the robot to the right 
-#         elif range <= distance:
-#             print("robot going right" + distance)
-#             time.sleep(1)
-
-#         elif 5 <= distance: #Stops the robot
-#             print("robot will stop" + distance)
-#             robot.stop()
-    
-#     print('Connection closed')
-#     print('<--------------------------------------->')
-
-# # ------------------------------------------ MAIN NAVIGATION CODE --------------------------------------------------
-# print("navigation started")
-
-# schedule.every(20).seconds.do(CollectUltrasonic)
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+#<---------------------------- Main code -------------------->
+while True:
+    CollectUltrasonic()
