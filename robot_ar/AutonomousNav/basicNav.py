2089df9701ccca7da16be1c6604cedbd352254cb
ArgenisW <argen1swong@gmail.com>
2022-04-22 23:02:35 -0400

    Updated BasicNav.py

--- robot_ar/AutonomousNav/basicNav.py
+++ robot_ar/AutonomousNav/basicNav.py
@@ -1,6 +1,8 @@
+from operator import truediv
 import motor_class
 import serial
 import time
+import schedule
 
 
 # Basic navigation using line as a input
@@ -13,31 +15,41 @@ RIGHT_TRIM = 0
 robot = motor_class.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
 # Arduino Serial Port Listener
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.reset_input_buffer()
-
-    while True:
-        # Define the variables that will be used for navigation
-        range = 30
-        number = ser.read()
-
-        if number != b'':
-            distance = int.from_bytes(number, byteorder='big')
-            # First conditional statement to move the robot forward 
-            if range >= distance:
-                print("robot forward" + distance)
-                time.sleep(1)
-
-            # Second conditional statement to move the robot to the right 
-            elif range <= distance:
-                print("robot going right" + distance)
-                time.sleep(1)
-
-            elif 5 <= distance: #Stops the robot
-                print("robot will stop" + distance)
-                robot.stop()
-                break
 
 
-#Loop that runs the code that will register the keyboard entry
+def CollectUltrasonic():
+    arduino = serial.Serial('/dev/ttyUSB0', 9600)
+    # Define the variables that will be used for navigation
+    range = 30
+    arduino_data = arduino.readline()
+
+    # distance = int(arduino_data, byteorder='big')
+    distance = int.from_bytes(arduino_data, byteorder='big')
+    print(distance)
+    # First conditional statement to move the robot forward 
+    if range >= distance:
+        print("robot forward" + distance)
+        time.sleep(1)
+
+    # Second conditional statement to move the robot to the right 
+    elif range <= distance:
+        print("robot going right" + distance)
+        time.sleep(1)
+
+    elif 5 <= distance: #Stops the robot
+        print("robot will stop" + distance)
+        robot.stop()
+    
+    arduino_data = 0
+    arduino.close()
+    print('Connection closed')
+    print('<--------------------------------------->')
+
+# ------------------------------------------ MAIN NAVIGATION CODE --------------------------------------------------
+print("navigation started")
+
+schedule.every(2).seconds.do(CollectUltrasonic)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
