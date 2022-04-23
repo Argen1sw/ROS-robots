9d70995748108f1869a1689ff307853707d49857
ArgenisW <argen1swong@gmail.com>
2022-04-23 01:54:20 -0400

    basicNav.py
    Basic navigation updated, it uses a ultrasonic sensor to navigate an initialize methods of the robot class

--- robot_ar/AutonomousNav/basicNav.py
+++ robot_ar/AutonomousNav/basicNav.py
@@ -1,4 +1,5 @@
 from operator import truediv
+from queue import Empty
 import motor_class
 import serial
 import time
@@ -13,43 +14,7 @@ RIGHT_TRIM = 0
 
 robot = motor_class.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
 
-# Arduino Serial Port Listener
-
-# if __name__ == '__main__':
-#     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-#     ser.reset_input_buffer()
-
-#     while True:
-#         ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-#         ser.reset_input_buffer()
-#         arduino = ser.read()
-#         if arduino != b'':
-#             arduino_data = int.from_bytes(arduino, byteorder='big')
-#             print(arduino_data)
-#             range = 30
-#             #First conditional statement to move the robot forward 
-#             print("starting the navigation loop")
-#             if arduino_data <= 5: #Stops the robot
-#                 print("robot will stop" + str(arduino_data))
-#                 robot.stop()
-#                 break
-
-#             elif range <= arduino_data:
-#                 print("robot forward" + str(arduino_data))
-#                 robot.forward(0.2, 0.2)
-#                 time.sleep(1)
-
-#             # Second conditional statement to move the robot to the right 
-#             elif range >= arduino_data:
-#                 print("robot going right" + str(arduino_data))
-#                 robot.right(0.2, 0.5)
-#                 time.sleep(1)
-
-#         time.sleep(2)
-
-
-
-def CollectUltrasonic():
+def collectUltrasonic():
 
     if __name__ == '__main__':
         ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
@@ -79,7 +44,16 @@ def CollectUltrasonic():
 
         time.sleep(2)
 
+# def collectGyroscope()
+    #this will be a function that will help with the angular displacement of the robot
+
+# def collectCameraData()
+    #Function that will collect the data of the camara and maybe used it
+
+# def a LiDAR function
 
 #<---------------------------- Main code -------------------->
 while True:
-    CollectUltrasonic()
+    collectUltrasonic()
+    if collectUltrasonic() == 0:
+        break
