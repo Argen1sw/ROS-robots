612df906b3c032ea2c0d65c6b804dc91336137bf
Ubuntu <ubuntu@ubuntu.lan>
2023-03-17 06:17:15 +0000

    testing branch
    Committer: Argenis <argen1swong@gmail.com>

---
+++ robot_ar/tests/game_controll_tests/device_print_input.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import inputs
+
+devices = inputs.devices.gamepads
+print(f"Connected devices: {devices}")
+
+gamepad = inputs.devices.gamepads[0]
+
+while True:
+    events = gamepad.read()
+    for event in events:
+        print(event.ev_type, event.code, event.state)
+
