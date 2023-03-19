0bd034d5a8ecbeba18499af4d5f31b52fa2da71c
ArgenisW <argen1swong@gmail.com>
2023-03-19 00:36:31 -0400

    fdasf

--- robot_ar/navigation/device_print_input.py
+++ robot_ar/navigation/device_print_input.py
@@ -10,5 +10,5 @@ gamepad = inputs.devices.gamepads[0]
 while True:
     events = gamepad.read()
     for event in events:
-        print("Type of data code:" ,type(event.code), "         Type of state code: ", type(event.state))
+        print(event.code)
 
